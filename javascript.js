f5fdb5bcbf8db8477142b6f5b8c5eb66bbfb48cc
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-03-21 11:33:43 +0300

    add ordering functionality

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+      
 // contact us function
 $(document).ready(function(){
     $(".myForm").submit(function(event){
@@ -11,50 +12,85 @@ $(document).ready(function(){
 })
 
 // ordering constructor
-class orderPizza {
-    constructor(size, crust, topping, numberOfPizza, deliver) {
+class Orderpizza {
+    constructor(size, crust, topping, numberOfPizza,) {
         this.size = size;
         this.crust = crust;
         this.topping = topping;
         this.numberOfPizza = numberOfPizza;
-        this.delivery = delivery;
+        
     }
 };
 
-// total price calculation
-// size calculation
-orderPizza.prototype.getSIzePrice = function(){
-    if(this.size === "small") {
-        return 500;
-    } else if(this.size === "medium")  {
-        return 800;
-    } else if(this.size === "large") {
-        return 1200;
-    } else{return "pick your option"}        
-    
-}
+
+
+//  user input
+$(document).ready(function(){
+    $(".form").submit(function (event){
+        event.preventDefault();    
+        let pizzaSize = $("#size option:selected").val();
+        let pizzaCrust = $("#crust option:selected").val();
+        let pizzaTopping = $("#topping option:selected").val();
+        let numberOfPizza = parseInt($("#number").val());
+        let userDelivery = $("#delivery").val();  
+        let totalPrice  = 0;
+        
+    if(pizzaSize === "small") {
+        totalPrice += 500;
+    } else if(pizzaSize === "medium")  {
+        totalPrice += 800;
+    } else if(pizzaSize === "large") {
+        totalPrice += 1200;
+    } 
 // crust calculation
-orderPizza.prototype.getCrust.Price = function () {
-    if(this.crust === "custom") {
-        return 250;
-    } else if(this.crust === "thin"){
-        return 100;
-    } else if(this.crust === "thick"){
-        return 150;
-    } else if( this.crust === "flatbread") {
-        return 250;
-    }else {return "pick your option"}
-}
+
+    if(pizzaCrust === "custom") {
+        totalPrice += 250;
+    } else if(pizzaCrust === "thin"){
+        totalPrice += 100;
+    } else if(pizzaCrust === "thick"){
+        totalPrice += 150;
+    } else if(pizzaCrust === "flatbread") {
+        totalPrice+= 250;
+    }
+
 
 // topping calculation
+ 
+     if(pizzaTopping === "pineapple") {
+         totalPrice += 150;
+     }else if(pizzaTopping === "sweetcorn") {
+         totalPrice += 150;
+     } else if(pizzaTopping === "olives") {
+         totalPrice += 150;
+     } else if(pizzaTopping === "macon") {
+         totalPrice += 150;
+     } else if(pizzaTopping === "mince") {
+         totalPrice += 150;
+     } else if(pizzaTopping === "chicken") {
+         totalPrice += 200;
+     } else if(pizzaTopping === "beef") {
+         totalPrice += 200;
+     } else if(pizzaTopping === "tomato") {
+         totalPrice += 80;
+     }else if(pizzaTopping === "onions") {
+         totalPrice += 80;
+     }else if(pizzaTopping === "mushrooms") {
+         totalPrice += 80;
+     }else if(pizzaTopping === "greenpepper") {
+         totalPrice += 80;
+     }   
+
+     let grandtotal = totalPrice * numberOfPizza
+     let deliveryTotal = grandtotal + 200;
+     if(userDelivery !=""){
+         alert(`Hi, you have ordered ${numberOfPizza} ${pizzaSize} pizza with ${pizzaCrust} crust and ${pizzaTopping} topping,your total bill will be ${deliveryTotal} and will be delivered to ${userDelivery} in 45 minutes. Thank you.`)
+     }else{
+     alert(`Hi, you have ordered ${numberOfPizza} ${pizzaSize} pizza with ${pizzaCrust} crust and ${pizzaTopping} topping,your total bill will be ${grandtotal} and will be ready in 25 minutes. Thank you.`)
+
+     }
+
+    })
+})
 
 
-// function delivery(){
-//     let location  = prompt("please enter your location")
-//     // let deliveryType = document.forms["form"]["delivery"].value;
-//    if(document.getElementById("flexRadioDefault1").checked){
-//        alert(location)
-//        return false
-//    }else("working on your order")
-// }
-// delivery()
